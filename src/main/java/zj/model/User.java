54fa4c0bade8c5d5b16a83656110f78f16ce8e97
lzj <371209704@qq.com>
2017-02-14 18:39:02 +0800

    asd

---
+++ src/main/java/zj/model/User.java
@@ -0,0 +1,27 @@
+package zj.model;
+
+/**
+ * Created by lzj on 2017/1/17.
+ */
+public class User extends ModelBase {
+
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
